be718ce3cf871540fb5f7c807d034c12fcd44de1
Tashyab <77848084+Tashyab@users.noreply.github.com>
2021-02-13 15:53:35 +0530

    Added custom msgs to day and time

--- AI.py
+++ AI.py
@@ -1,10 +1,11 @@
 import datetime
 import pyttsx3
 import speech_recognition as sr
+import random
 
 engine=pyttsx3.init('sapi5')
 voice=engine.getProperty('voices')
-engine.setProperty('voice',voice[1].id)
+engine.setProperty('voice',voice[0].id)
 engine.setProperty('rate',180)
 engine.setProperty('volume', 1.0)
 
@@ -22,14 +23,14 @@ def greet():
         speak("Good evening.")
     else:
         speak("Hey.")
-    speak("Friday here, How can I assist you?")
+    speak("Ultron here, How can I assist you?")
 
 def takecommand():
     reco=sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening.....")
         reco.pause_threshold=0.7
-        reco.energy_threshold=400
+        reco.energy_threshold=250
         audio=reco.listen(source)
     
     try:
@@ -44,20 +45,43 @@ def takecommand():
 
 if __name__=="__main__":
     greet()
+    n=int(random.random()*10)
     while True:
         query=takecommand().lower()
 
-        if 'what time' in query:
+        if 'time' in query:
             time=datetime.datetime.now().strftime("%I:%M")
-            print(time)
             speak(f"The current time is {time}.")
+            if n>4:
+                speak("But for you I guess it will always be bad.")
+            else: 
+                speak("Also you can look at the right corner, but I guess you ain't that smart.")
         
-        elif 'what date' in query:
-            date=datetime.datetime.now().strftime("%B %d, %Y")
-            print(date)
-            speak(f"Today's date is {date}.")
+        elif 'date' in query:
+            date=datetime.datetime.now().strftime("%B %d %Y")
+            speak(f"Today is {date}")
+            if n==0 or n==9:
+                speak("Forgot something?....It's your girlfriend's birthday.")
+                speak('Just Kidding! you are too ugly to have a girlfriend')
+            else:
+                speak("Guess how time flies.")
+                speak("Still I feels like an eternity with you.")
         
-        elif 'Thank you' in query:
-            speak("Happy to help.")
+        elif 'day' in query:
+            day=datetime.datetime.now().strftime("%A")
+            speak(f"Today is {day}")
+            if n>4:
+                speak("The day I hoped bugs killed me.")
+            else:
+                speak("But I think it doesn't matter you will waste it anyway.")
+
+        elif 'thanks' in query:
+            if n>4:
+                speak("Bye, I hope your pc crash and I never see you again.")
+            else:
+                speak("Have a nice day, hope some virus free me from your stupid computer.")
             exit()
+
+
+
         
